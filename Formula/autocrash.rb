5c7de1c34114876bf4bbf71044a0fe4ed21b35b7
zhang xiuming <zhangxiuming@zepp.com>
2022-08-22 14:40:29 +0800

    test

--- Formula/autocrash.rb
+++ Formula/autocrash.rb
@@ -1,7 +1,7 @@
 class Autocrash < Formula
   desc "华米iOS崩溃自动解析脚本"
   homepage "https://github.com/roshanman/my-shells"
-  url "https://github.com/roshanman/my-shells.git", tag: "1.0.2"
+  url "https://github.com/roshanman/my-shells.git", tag: "1.0.3"
   version "1.0.1"
   license "MIT"
 
