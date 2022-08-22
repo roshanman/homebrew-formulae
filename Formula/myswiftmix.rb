aa2f9f01a3246c813561f4d1c93c9739a03f10c4
zhang xiuming <zhangxiuming@zepp.com>
2022-08-22 09:24:05 +0800

    test

--- Formula/myswiftmix.rb
+++ Formula/myswiftmix.rb
@@ -1,8 +1,8 @@
 class MySwiftmix < Formula
   desc "Import files into your Swift scripts!"
-  homepage "https://github.com/mrtokii/swiftmix"
+  homepage "https://github.com/roshanman/swiftmix"
   url "https://github.com/roshanman/swiftmix.git", tag: "1.0.1"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   depends_on xcode: ["13.0", :build]
