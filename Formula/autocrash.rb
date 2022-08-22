397c3dc626c5cadd06ba63c40c5c915a9cbcf1f7
zhang xiuming <zhangxiuming@zepp.com>
2022-08-22 14:09:41 +0800

    autocrash

---
+++ Formula/autocrash.rb
@@ -0,0 +1,18 @@
+class Autocrash < Formula
+  desc "华米iOS崩溃自动解析脚本"
+  homepage "https://github.com/roshanman/my-shells"
+  url "https://github.com/roshanman/my-shells.git", tag: "1.0.1"
+  version "1.0.1"
+  license "MIT"
+
+  depends_on xcode: ["13.0", :build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "touch" "testfile"
+    system "#{bin}/swiftmix" "testfile"
+  end
+end
